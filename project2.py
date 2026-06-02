e6312d90fc917a3d595904aad3e007be1aa03674
Bruhbro22 <donmadhu411@gmail.com>
2026-06-02 10:14:12 -0500

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,15 @@
+birthday_1="july"
+birthday_2="august"
+birthday_3="september"
+birthday_4="june"
+birthday_5="may"
+
+print("Hello I am Raj and let's see my friend's birthdays!")
+print("My 1st friend, Barghav, his birthday is on",birthday_1 )
+print("My 2nd friend, Lokesh, his birthday is on",birthday_2 )
+print("My 3rd friend, Arjun, his birthday is on",birthday_3 )
+print("My 4th friend, Jagdish, his birthday is on",birthday_4 )
+print("My 5th friend, Anish, his birthday is on",birthday_5 )
+
+answer=input("Do you like my project: ")
+print("Good to know!")
